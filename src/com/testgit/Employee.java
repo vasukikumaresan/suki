fe3b59afe82574b3eae387721bf9d6e6ed4caae1
vasukikumaresan <vasukikumaresan1211@gmail.com>
2021-12-11 04:24:00 +0530

    15Lakhs

--- src/com/testgit/Employee.java
+++ src/com/testgit/Employee.java
@@ -1,19 +1,28 @@
 package com.testgit;
 
 public class Employee {
-	
 
+	
 	public class SamplePr {
-	public void main(String[] args) {
-		int a=10;
-		while(a<=25) {
-			System.out.println(a);
-			a++;
+		
+		public void close() {
+			System.out.println("print all git bash");
 		}
-	}
+		public void quit() {
+			System.out.println("15 Lakhs per annum");
+		}
+
+		public void main(String[] args) {
+			int a = 10;
+			while (a <= 25) {
+				System.out.println(a);
+				a++;
+			}
+		}
+		
 	}
 
 	
 	
-
+	
 }
