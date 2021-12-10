060bffe298fd6370ccd0020034cbbf705585d696
vasukikumaresan <vasukikumaresan1211@gmail.com>
2021-12-11 03:13:24 +0530

    tester

---
+++ src/com/testgit/Employee.java
@@ -0,0 +1,19 @@
+package com.testgit;
+
+public class Employee {
+	
+
+	public class SamplePr {
+	public void main(String[] args) {
+		int a=10;
+		while(a<=25) {
+			System.out.println(a);
+			a++;
+		}
+	}
+	}
+
+	
+	
+
+}
